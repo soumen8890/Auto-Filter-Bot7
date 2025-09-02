e1253929d2e6354774d7ca15ea175017a1db506e
soumen8890 <130748304+soumen8890@users.noreply.github.com>
2025-09-02 17:57:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,7 +60,7 @@ AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "-1002389404459") # add multiple chan
 AUTH_REQ_CHANNEL = environ.get('AUTH_REQ_CHANNEL', '-1002389404459') # add multiple channels here, separated by single space
 
 IS_VERIFY = is_enabled('IS_VERIFY', False)
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', ''))
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-100'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-100'))
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
 
